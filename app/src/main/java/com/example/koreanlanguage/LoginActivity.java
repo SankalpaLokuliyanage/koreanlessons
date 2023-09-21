c5dbcfc8c339d4158d672f5bd1c77d705fec2030
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-09-21 15:24:30 +0900

    login successfull

--- app/src/main/java/com/example/koreanlanguage/LoginActivity.java
+++ app/src/main/java/com/example/koreanlanguage/LoginActivity.java
@@ -1,14 +1,108 @@
 package com.example.koreanlanguage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText rEmail, rPass;
+    Button login;
+
+    ProgressDialog progressDialog;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // direct to the signup
+        findViewById(R.id.toRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                finish();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        rEmail = findViewById(R.id.signin_email);
+        rPass = findViewById(R.id.signin_password);
+        login = findViewById(R.id.signInText);
+
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging User...");
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = rEmail.getText().toString().trim();
+                String password = rPass.getText().toString().trim();
+
+                //validate
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    rEmail.setError("Invalid Email");
+                    rEmail.setFocusable(true);
+                }
+                else if (password.length() < 6) {
+                    rPass.setError("Password is must be more than 6 Characters");
+                    rPass.setFocusable(true);
+                }
+                else {
+                    loginUser (email, password);
+                }
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String password) {
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
